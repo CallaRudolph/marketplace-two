0e4b5a029764c52398b6a978ab63d2b402349770
Nathan Stewart <stewart.nathang@gmail.com>
2017-08-21 21:07:05 -0700

    Add dynamic routing for albums
    the rest is a test to see if i am doing this commit correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     AboutComponent,
-    MarketplaceComponent
+    MarketplaceComponent,
+    AlbumDetailComponent,
+    // FormsModule
   ],
   imports: [
     BrowserModule,
